60ea733c821dfe704175bf4fd2722af5a2c3b3ca
Chen Tingjie <tingjie.chen@intel.com>
2016-10-20 15:33:47 +0800

    Update fundamental information with date
    Signed-off-by: Chen Tingjie <tingjie.chen@intel.com>

--- strategy.py
+++ strategy.py
@@ -27,12 +27,12 @@ class base_strategy():
     def __init__(self):
         self.name = "base"
         self.fundaLib = {}
-        self.fundaLib[fundaType.performance] = funda_cell(ts.get_report_data, "performance_report.csv")
-        self.fundaLib[fundaType.profit] = funda_cell(ts.get_profit_data, "profit_report.csv")
-        self.fundaLib[fundaType.operation] = funda_cell(ts.get_operation_data, "operation_report.csv")
-        self.fundaLib[fundaType.growth] = funda_cell(ts.get_growth_data, "growth_report.csv")
-        self.fundaLib[fundaType.debtpaying] = funda_cell(ts.get_debtpaying_data, "debtpaying_report.csv")
-        self.fundaLib[fundaType.cashflow] = funda_cell(ts.get_cashflow_data, "cashflow_report.csv")
+        self.fundaLib[fundaType.performance] = funda_cell(ts.get_report_data, "performance_report")
+        self.fundaLib[fundaType.profit] = funda_cell(ts.get_profit_data, "profit_report")
+        self.fundaLib[fundaType.operation] = funda_cell(ts.get_operation_data, "operation_report")
+        self.fundaLib[fundaType.growth] = funda_cell(ts.get_growth_data, "growth_report")
+        self.fundaLib[fundaType.debtpaying] = funda_cell(ts.get_debtpaying_data, "debtpaying_report")
+        self.fundaLib[fundaType.cashflow] = funda_cell(ts.get_cashflow_data, "cashflow_report")
 
     def get_stock_basics(self):
         info = ts.get_stock_basics()
@@ -41,6 +41,6 @@ class base_strategy():
 
     def get_fundamental_info(self, type, year, quarter):
         info = self.fundaLib[type].get_callback()(year, quarter)
-        info.to_csv("./output/" + self.fundaLib[type].get_savefile())
+        info.to_csv("./output/" + self.fundaLib[type].get_savefile() + "_%s_%s"%(str(year), str(quarter)) + ".csv")
         return info
 
