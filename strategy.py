4817b6b52ae9e037747d56da6bd7646cc91d393f
Chen Tingjie <tingjie.chen@intel.com>
2016-10-20 15:20:20 +0800

    Update toprank with fundamental basics
    Signed-off-by: Chen Tingjie <tingjie.chen@intel.com>

--- strategy.py
+++ strategy.py
@@ -1,9 +1,46 @@
 #!/usr/bin/env python
 
 import sys, os, string
+from enum import Enum
+import tushare as ts
+
+class fundaType(Enum):
+    performance = 1
+    profit = 2
+    operation = 3
+    growth = 4
+    debtpaying = 5
+    cashflow = 6
+
+class funda_cell():
+    def __init__(self, callback, savefile):
+        self._callback = callback
+        self._savefile = savefile
+
+    def get_callback(self):
+        return self._callback
+
+    def get_savefile(self):
+        return self._savefile
 
 class base_strategy():
     def __init__(self):
-        self.name = "root"
+        self.name = "base"
+        self.fundaLib = {}
+        self.fundaLib[fundaType.performance] = funda_cell(ts.get_report_data, "performance_report.csv")
+        self.fundaLib[fundaType.profit] = funda_cell(ts.get_profit_data, "profit_report.csv")
+        self.fundaLib[fundaType.operation] = funda_cell(ts.get_operation_data, "operation_report.csv")
+        self.fundaLib[fundaType.growth] = funda_cell(ts.get_growth_data, "growth_report.csv")
+        self.fundaLib[fundaType.debtpaying] = funda_cell(ts.get_debtpaying_data, "debtpaying_report.csv")
+        self.fundaLib[fundaType.cashflow] = funda_cell(ts.get_cashflow_data, "cashflow_report.csv")
 
+    def get_stock_basics(self):
+        info = ts.get_stock_basics()
+        info.to_csv("./output/stock_basics.csv")
+        return info
+
+    def get_fundamental_info(self, type, year, quarter):
+        info = self.fundaLib[type].get_callback()(year, quarter)
+        info.to_csv("./output/" + self.fundaLib[type].get_savefile())
+        return info
 
--- toprank.py
+++ toprank.py
@@ -6,7 +6,19 @@ __mailbox__='silverhandy@hotmail.com'
 import os, sys, re, getopt, json, datetime
 from collections import OrderedDict
 import tushare as ts
+import pandas as pd
 import strategy
+
+class toprank_cell():
+    def __init__(self, stockId, increaseRate):
+        self._stockId = stockId
+        self._increaseRate = increaseRate
+
+    def get_stockId(self):
+        return self._stockId
+
+    def get_increase_rate(self):
+        return self._increaseRate
 
 class toprank_unit():
     def __init__(self, days, whichDay, rankStocks):
@@ -28,22 +40,22 @@ class toprank_unit():
         increase_rate = self.get_increase(df)
         if (increase_rate <= 0):
             return
-        element = (stockId, increase_rate)
+        cell = toprank_cell(stockId, increase_rate)
         if (len(self.rankBowl) < self.rankStocks):
-            self.rankBowl.append(element)
+            self.rankBowl.append(cell)
         else:
-            self.rankBowl.sort(key=lambda x:x[1])
+            self.rankBowl.sort(key=lambda x:x.get_increase_rate())
             #self.print_toprank_unit()
-            if (self.rankBowl[0][1] < increase_rate):
+            if (self.rankBowl[0].get_increase_rate() < increase_rate):
                 #print("\n************* Unit:", self.whichUnit, "Pop:", self.rankBowl[0][1], "Append:", element[1])
                 self.rankBowl.pop(0)
-                self.rankBowl.append(element)
+                self.rankBowl.append(cell)
 
     def find_and_pop_unit(self, stockId):
         weightIncrease = 0.0
         for i in self.rankBowl:
-            if stockId == i[0]:
-                weightIncrease += i[1]/(self.whichUnit+1)
+            if stockId == i.get_stockId():
+                weightIncrease += i.get_increase_rate()/(self.whichUnit+1)
                 self.rankBowl.pop(self.rankBowl.index(i))
                 break
         return weightIncrease
@@ -52,7 +64,7 @@ class toprank_unit():
         return 0 == len(self.rankBowl)
 
     def get_first_stockId(self):
-        return self.rankBowl[0][0]
+        return self.rankBowl[0].get_stockId()
 
     def print_toprank_unit(self):
         for i in self.rankBowl:
@@ -93,6 +105,8 @@ datetime.date.today().day)
         (start_day, end_day) = self.pick_date_from_days(days)
         #print("high: stockId", stockId, "days", days, "start_day", start_day, "end_day", end_day)
         df = ts.get_h_data(stockId, start=start_day, end=end_day)
+        if df is None:
+            return False
         period_high = df['high'].max()
         today_high = df.iloc[0]['high']
         if today_high >= period_high:
@@ -109,11 +123,11 @@ datetime.date.today().day)
        	    self.units[i].add_toprank_increase_unit(stockId, df)
 
     def toprank_loop_stocks(self):
-        info = ts.get_stock_basics()
-        #i = 50
+        info = super().get_stock_basics()
+        i = 50
         for eachStockId in info.index:
-            #i = i-1
-            #if (i < 0): break
+            i = i-1
+            if (i < 0): break
             if self.is_break_high(eachStockId, self.breakHighDays):
                 #print("Break_high: ", eachStockId, info.ix[eachStockId])
                 self.breakHighThrone.append(eachStockId)
@@ -137,32 +151,49 @@ datetime.date.today().day)
             while(not self.units[i].is_bowl_empty()):
                 stockId = self.units[i].get_first_stockId()
                 weightIncrease = self.find_and_pop_toprank_unit(stockId)
-                perl = (stockId, weightIncrease)
+                perl = toprank_cell(stockId, weightIncrease)
                 self.rankCrown.append(perl)
 
     def eliminate_without_break_high(self):
         for i in self.rankCrown:
-            if i[0] not in self.breakHighThrone:
+            if i.get_stockId() not in self.breakHighThrone:
                 self.rankCrown.pop(self.rankCrown.index(i))
 
     def select_toprank_stocks(self):
         self.load_recent_stock_info()
         self.merge_toprank_crown()
         self.eliminate_without_break_high()
-        self.rankCrown.sort(key=lambda x:-x[1])
+        self.rankCrown.sort(key=lambda x:-x.get_increase_rate())
         print("$$$$$$$$$$$ rankCrown:", self.rankCrown)
 
+    def get_fundamental_all(self, year, quarter):
+        super().get_stock_basics()
+        super().get_fundamental_info(strategy.fundaType.performance, year, quarter)
+        super().get_fundamental_info(strategy.fundaType.profit, year, quarter)
+        super().get_fundamental_info(strategy.fundaType.operation, year, quarter)
+        super().get_fundamental_info(strategy.fundaType.growth, year, quarter)
+        super().get_fundamental_info(strategy.fundaType.debtpaying, year, quarter)
+        super().get_fundamental_info(strategy.fundaType.cashflow, year, quarter)
+
 if __name__ == "__main__":
-    opts, args = getopt.getopt(sys.argv[1:], "hp:")
+    opts, args = getopt.getopt(sys.argv[1:], "hsf:")
     tr = toprank_strategy()
     for op, value in opts:
-        if op == "-p":
-            path = value
-            print("Get file {0}".format(path))
+        if op == "-f":
+            div = value.find(',')
+            year = int(value[0:div])
+            quarter = int(value[div+1:])
+            print("Parse date year:", year, "quarter:", quarter)
+            tr.get_fundamental_all(year, quarter)
+            sys.exit()
+        elif op == "-s":
+            print("Selecting toprank stocks...")
+            tr.select_toprank_stocks()
             sys.exit()
         elif op == "-h":
-            print("Usage: python topranks.py -p YOUR_FULL_PATH_NAME")
+            print("python3 topranks.py -s # select toprank stocks")
+            print("python3 topranks.py -f 2015,4 # get fundamental info")
             print("version of tushare: %s"% ts.__version__)
-            tr.select_toprank_stocks()
+            print("version of pandas: %s"% pd.__version__)
             sys.exit()
 
